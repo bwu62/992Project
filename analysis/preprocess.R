e9a45474f66c3296c1480f86a30ac43d277fadd7
Bi Cheng Wu <bwu62@wisc.edu>
2020-10-04 19:14:22 -0500

    added preprocess script, added processed & compressed data, update site first blog page

---
+++ analysis/preprocess.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+# # read in data and convert columns
+# principals =
+#   read_tsv("data/title.principals.tsv.gz",na="\\N",quote="") %>% 
+#   select(-c(ordering,job,characters)) %>% 
+#   filter(category %in% c("self","actor","actress")) %>% 
+#   mutate_if(is_character,as_factor) %>% 
+#   select(-category)
+# 
+# # save result ot be easily loaded
+# save(principals,file="data/principals.Rdata.xz",compress="xz",compression_level=9)
+
+load("data/principals.Rdata.xz")
+
+
