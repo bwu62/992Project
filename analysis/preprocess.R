1a3fb25f11f5e96ca46953429362b5e9402775b8
Bi Cheng Wu <bwu62@wisc.edu>
2020-10-05 14:05:50 -0500

    added graph generating code

--- analysis/preprocess.R
+++ analysis/preprocess.R
@@ -1,12 +1,14 @@
 library(tidyverse)
+library(igraph)
 
 # # read in data and convert columns
 # principals =
-#   read_tsv("data/title.principals.tsv.gz",na="\\N",quote="") %>% 
-#   select(-c(ordering,job,characters)) %>% 
-#   filter(category %in% c("self","actor","actress")) %>% 
-#   mutate_if(is_character,as_factor) %>% 
-#   select(-category)
+#   read_tsv("data/title.principals.tsv.gz",na="\\N",quote="") %>%
+#   select(-c(ordering,job,characters)) %>%
+#   filter(category %in% c("self","actor","actress")) %>%
+#   mutate_if(is_character,as_factor) %>%
+#   select(-category) %>% 
+#   distinct
 # 
 # # save result ot be easily loaded
 # save(principals,file="data/principals.Rdata.xz",compress="xz",compression_level=9)
@@ -16,3 +18,8 @@ load("data/principals.Rdata.xz")
 
 # check size
 print(object.size(principals),units="Mb")
+
+# create graph using naive (i.e. slow) method since I can't figure out how to optimize it
+imdb = full_join(principals,principals,c('tconst'='tconst'))[,c(2,3,1)] %>%
+  filter(nconst.x!=nconst.y) %>%
+  graph_from_data_frame(directed=F)
