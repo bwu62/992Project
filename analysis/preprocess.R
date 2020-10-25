c72464261be1a146f7a19d9f154dcd24b9445788
Ckardatzke <kardatzke@wisc.edu>
2020-10-25 13:46:06 -0500

    Calculated more actors' mean degrees of seperation

--- analysis/preprocess.R
+++ analysis/preprocess.R
@@ -37,7 +37,7 @@ library(igraph)
 # save(list=ls(),file="data/principals+names.Rdata.xz",compress="xz",compression_level=9)
 
 # load file and check size
-load("data/principals+names.Rdata.xz")
+load("analysis/data/principals+names.Rdata.xz")
 
 # get title and actor counts (how many actors/titles were associated with a particular title/actor)
 actor.counts = principals %>% group_by(tconst) %>% summarise(n=n()) %>% pull(n,name=tconst)
@@ -64,3 +64,34 @@ head(sort(imdb.comp$csize,decreasing=T),10)
 imdb.comp$membership[imdb.comp$membership == 1]
 imdb2 = induced_subgraph(imdb,names(imdb.comp$membership[imdb.comp$membership == 1]))
 
+#Degrees of seperation of every actor from Kevin Bacon (nm0000102)
+degrees_seperation<-distances(imdb, v = "nm0000102")
+
+#Percent of actors who are connected with Kevin bacon
+print(sum(is.finite(degrees_seperation))/length(degrees_seperation))
+
+#See distribution of degrees of seperation
+as.data.frame(table(degrees_seperation))
+
+#Use this to look up someone's degrees of seperation from Bacon
+#print(degrees_seperation[which(colnames(degrees_seperation)=="nm0000102")])
+
+#Get all actors one degree away from Bacon
+oneAway <- colnames(degrees_seperation)[which(degrees_seperation %in% 1)]
+
+actorList <- list()
+actorMean <- list()
+
+#Calculate every one of these actor's mean degrees of seperation
+for (i in 1:length(oneAway)) {
+  deg_sep<-distances(imdb, v = oneAway[i])
+  actorList[[i]]<-oneAway[i]
+  # print(sum(is.finite(deg_sep))/length(deg_sep))
+  #print(as.data.frame(table(deg_sep)))
+  deg <- deg_sep[is.finite(deg_sep)]
+  actorMean[[i]]<-mean(deg)
+}
+df <- data.frame(matrix(unlist(actorMean), nrow=231, byrow=T),stringsAsFactors=FALSE)
+df$actor <- unlist(actorList)
+colnames(df)[1] <- "mean"
+avgDegrees<-df[order(df$mean),]
