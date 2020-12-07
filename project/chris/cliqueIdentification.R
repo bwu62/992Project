1d2c6c5363def313034bc7c0e5b5b6b51014eb8f
Ckardatzke <kardatzke@wisc.edu>
2020-12-07 14:13:07 -0600

    Ran VSP on subset incidence matrix

--- project/chris/cliqueIdentification.R
+++ project/chris/cliqueIdentification.R
@@ -10,7 +10,8 @@ pacman::p_load(Matrix,igraph,tidyverse,ggplot2,scales)
 
 # load the compressed binary file
 load("../eng_principals2+names.Rdata")
-
+load("fa2.clusters.Rdata")
+fa2.clusters<- as.data.frame(fa2.clusters)
 # check loaded files
 ls()
 
@@ -46,8 +47,10 @@ movie.graph = graph_from_adjacency_matrix(AAT, mode = c("directed", "undirected"
                                                        "max", "min", "upper", "lower", "plus"), weighted = NULL,
                                          diag = TRUE, add.colnames = NULL, add.rownames = NA)
 
-#Every actor's degrees of seperation of every actor from 12 Angry Men (tt0050083)
-degrees.from.tam<-as.data.frame(distances(movie.graph, v = "tt0050083"))
+#Every movie's degrees of seperation of every actor from 12 Angry Men (tt0050083)
+degrees.from.tam<-as.data.frame(t(distances(movie.graph, v = "tt0050083")))
+#Change column name
+colnames(degrees.from.tam)<-c("DegreesFromTam")
 
 #Every actor's degrees
 actor.degrees<-as.data.frame(degree(actor.graph, v = V(actor.graph), mode = c("all", "out", "in", "total"),
@@ -56,11 +59,26 @@ actor.degrees<-as.data.frame(degree(actor.graph, v = V(actor.graph), mode = c("a
 colnames(actor.degrees)<-c("degrees")
 actor.degrees$degress.from.roberts <- degrees.from.roberts$DegreesFromRoberts
 
-#Ever actor's coreness
+#Every actor's coreness
 actor.coreness<- as.data.frame(coreness(actor.graph, mode = "all"))
 colnames(actor.coreness)<-c("coreness")
 
 actor.degrees$coreness <- actor.coreness$coreness
 
+#Every movie's degrees
+movie.degrees<-as.data.frame(degree(movie.graph, v = V(movie.graph), mode = c("all", "out", "in", "total"),
+                                    loops = TRUE, normalized = FALSE))
 
+colnames(movie.degrees)<-c("degrees")
+movie.degrees$degress.from.tam <- degrees.from.tam$DegreesFromTam
+
+#Every movie's coreness
+movie.coreness<- as.data.frame(coreness(movie.graph, mode = "all"))
+colnames(movie.coreness)<-c("coreness")
+
+movie.degrees$coreness <- movie.coreness$coreness
+movie.degrees$cluster <- fa2.clusters$cluster
+movie.degrees$cluster.value <- fa2.clusters$value
+
+write.csv(movie.degrees, "movie.statistics.csv")
 
